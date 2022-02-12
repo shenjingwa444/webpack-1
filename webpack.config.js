3b00951688afacb8c9b9cb741522a980db22c47c
jasoncheng <shenjingwa444@qq.com>
2022-02-12 20:24:05 +0800

    update css

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,34 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
-    static: './dist',
+    static: "./dist",
   },
-  plugins: [new HtmlWebpackPlugin({
-    title: 'my app',
-    filename: 'index.html',
-    template: 'src/index.html'
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "my app",
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+    // new MiniCssExtractPlugin({
+    //     filename: "[name].[contenthash].css",
+    //     chunkFilename: "[id].[contenthash].css",
+    //     ignoreOrder: false,
+    //   }),
+  ],
   module: {
     rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"],
       },
     ],
   },
---
+++ webpack.config.prod.js
@@ -0,0 +1,44 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "my app",
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
+      ignoreOrder: false,
+    }),
+  ],
+  module: {
+    rules: [
+        {
+            test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../",
+            },
+          },
+          "css-loader",
+        ],
+      },
+    ],
+  },
+};
